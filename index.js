a155a07dd68f92037d42f1efd346bd972ae724e3
JulianaMusiimenta <julianamusiimenta0@gmail.com>
2025-02-04 14:41:43 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function sum(a,b) {
+    return a + b;
+}
+console.log("sum of 2, and 3", sum(2,3));
+console.log("sum of 4, and 7", sum(4,7));
+
+var x;
+let y;
+x=5
+y=7
+let z=x+y;
+let n=x*y;
+let b=x-y;
+
+console.log(z, 'z= addition')
+console.log(m, 'm= multiplication')
+console.log(b, 'subtraction')
